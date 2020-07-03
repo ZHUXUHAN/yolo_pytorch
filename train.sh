79871f4843fe0cfed28aafc8273a632f4b044bef
zhuxuhan <xiaozhang@deepglint.com>
2020-07-03 01:11:00 +0000

    update train.sh

--- train.sh
+++ train.sh
@@ -1,1 +1,2 @@
-python train.py --data coco2014.data --weights 'weights/darknet53.conv.74' --batch-size 16 --cfg yolov3-spp.cfg
+python train.py --data coco2014.data --weights 'weights/darknet53.conv.74' --batch-size 16 --cfg yolov3-spp.cfg \
+--cache-images
